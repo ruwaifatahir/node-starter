f455e4bae422a20a3e70f7c21dd648e2c496d311
ruwaifatahir <ruwaifatahirr@gmail.com>
2025-07-13 16:19:28 +0500

    rafactor codebase

--- prisma/migrations/20250625053909_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Article" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "body" TEXT,
-    "authorId" INTEGER NOT NULL,
-
-    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
-);
